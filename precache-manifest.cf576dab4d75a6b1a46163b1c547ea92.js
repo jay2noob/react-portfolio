fae5d44bffdcb6b0b3df45721d0dcf5d78a12cc6
John Fields <jjfields24@outlook.com>
2020-04-12 09:37:51 -0400

    Updates

---
+++ precache-manifest.cf576dab4d75a6b1a46163b1c547ea92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608959b6025929b5134c077f31ccfea7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7d8af1cdb3ee6fdd79ed",
+    "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf75752e30b37e4ee8f0",
+    "url": "/react-portfolio/static/js/2.7a5adea5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.7a5adea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8af1cdb3ee6fdd79ed",
+    "url": "/react-portfolio/static/js/main.ef8ad157.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  }
+]);
